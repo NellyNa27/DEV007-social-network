2e438f43d9ec2e25a562f1f2beb13bd0ee922adc
NellyNa27 <nlly.rro@gmail.com>
2023-07-03 08:53:29 -0600

    cambios Nelly previos a Isa

--- src/components/home.js
+++ src/components/home.js
@@ -1,27 +1,24 @@
 //export para tenerlo accesible, se crean los botones de acceso: registro e inicio sesión
 //onNavigate, se crea por parametro y argumento.
 export const home = (onNavigate) => {
-  const homeDiv = document.createElement("div");
-  const buttonLogin = document.createElement("button");
-  const buttonRegister = document.createElement("button");
-  const buttonGoogle = document.createElement("img");
+  const homeDiv = document.createElement('div');
+  const buttonLogin = document.createElement('button');
+  const buttonRegister = document.createElement('button');
+  const buttonGoogle = document.createElement('img');
 
+  buttonRegister.textContent = 'Regístrate';
+  buttonRegister.classList = 'buttonsHome';
+  buttonLogin.textContent = 'Inicia Sesión';
+  buttonLogin.classList = 'buttonsHome';
+  buttonGoogle.classList = 'logoG';
+  buttonGoogle.src = '/images/btngo2.png';
 
-  buttonRegister.textContent = "Regístrate";
-  buttonRegister.classList = "buttonsHome";
-  buttonLogin.textContent = "Inicia Sesión";
-  buttonLogin.classList = "buttonsHome";
-  buttonGoogle.classList = "logoG";
-  buttonGoogle.src = "/images/btngo2.png";
-  
-  
-  buttonLogin.addEventListener("click", () => onNavigate("/login"));
-  buttonRegister.addEventListener("click", () => onNavigate("/register"));
-
+  buttonLogin.addEventListener('click', () => onNavigate('/login'));
+  buttonRegister.addEventListener('click', () => onNavigate('/register'));
 
   homeDiv.appendChild(buttonRegister);
   homeDiv.appendChild(buttonLogin);
   homeDiv.appendChild(buttonGoogle);
-  
+
   return homeDiv;
 };
--- src/components/recommendations.js
+++ src/components/recommendations.js
@@ -1,54 +1,56 @@
-import { createPost, enlistarPost } from "../lib";
+import { createPost, enlistarPost } from '../lib';
 
 export const recommendations = (onNavigate) => {
-  const recommendationsDiv = document.createElement("div");
-  const recommendationsTitle = document.createElement("h1");
-  const postContent = document.createElement("input");
-  const buttonPost = document.createElement("button");
-  const backToTheWall = document.createElement("button");
-
+  const recommendationsDiv = document.createElement('div');
+  const recommendationsTitle = document.createElement('h1');
+  const postContent = document.createElement('input');
+  const buttonPost = document.createElement('button');
+  const backToTheWall = document.createElement('button');
 
   //recomendationsDiv.innerHTML += `
   //<textarea id="textareaPost"></textarea>
   //<button  id="buttonAddPost">Agregar Recomendación</button>
   //`;
 
-  const postsDiv = document.createElement("div");
-  const postDiv = document.createElement("div");
+  const postsDiv = document.createElement('div');
+  const postDiv = document.createElement('div');
 
-  recommendationsDiv.classList = "recommendations";
-  recommendationsTitle.classList = "title";
-  recommendationsTitle.textContent = "Recomendaciones";
-  postContent.type = "text";
-  buttonPost.classList = "buttons";
-  buttonPost.textContent = "Subir una recomendación";
-  backToTheWall.classList = "buttons";
-  backToTheWall.textContent = "Cerrar Sesión";
-  backToTheWall.addEventListener("click", () => onNavigate("/home"));
+  recommendationsDiv.classList = 'recommendations';
+  recommendationsTitle.classList = 'title';
+  recommendationsTitle.textContent = 'Recomendaciones';
+  postContent.type = 'text';
+  buttonPost.classList = 'buttons';
+  buttonPost.textContent = 'Subir una recomendación';
+  backToTheWall.classList = 'buttons';
+  backToTheWall.textContent = 'Cerrar Sesión';
+  backToTheWall.addEventListener('click', () => onNavigate('/home'));
 
-  buttonPost.addEventListener("click", () => {
+  buttonPost.addEventListener('click', () => {
     if (postContent.value.length < 1) {
-      alert("Por favor ingresa algun texto para poder postear");
+      alert('Por favor ingresa algun texto para poder postear');
     } else {
       createPost(postContent.value)
         .then(() => {
-          alert("Post creado");
-          postContent.value = "";
+          alert('Post creado');
+          postContent.value = '';
         })
         .catch((error) => {
-          alert("error al subir el post, intente de nuevo");
+          alert('error al subir el post, intente de nuevo');
         });
     }
   });
-//hacer referencia con Doc Data para entrar a cada registro, recorrer cada registro para mostrarlo ... revisar documentacion para traer textos 
+  //hacer referencia con Doc Data para entrar a cada registro, recorrer cada registro para mostrarlo ... revisar documentacion para traer textos
   enlistarPost((callback) => {
     console.log(callback);
-    callback.forEach(element => {
-      console.log (element.data()); //crear contenedores en html para visualizar cada post  
-    const post = document.createElement("div");
-    post.classList = "buttons"
-    post.appendChild(document.createTextNode(element.data().content));
-    recommendationsDiv.appendChild (post);
+    //Limpiamos el div
+    postsDiv.innerHTML = '';
+    callback.forEach((element) => {
+      console.log(element.data()); 
+      //crear contenedores en html para visualizar cada post
+      const post = document.createElement('div');
+      post.classList = 'buttons';
+      post.appendChild(document.createTextNode(element.data().content));
+      postsDiv.appendChild(post);
     });
   }),
   recommendationsDiv.appendChild(recommendationsTitle);
@@ -56,8 +58,6 @@ export const recommendations = (onNavigate) => {
   recommendationsDiv.appendChild(buttonPost);
   recommendationsDiv.appendChild(backToTheWall);
   recommendationsDiv.appendChild(postsDiv);
-
-
 
   return recommendationsDiv;
 };
